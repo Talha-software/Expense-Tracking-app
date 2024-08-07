21fd042790a90f41a3e2e1d149aa1822eddf2a33
Talha Sajjad <talha.sajjad.software@gmail.com>
2024-08-08 00:28:05 +0500

    made table structure

---
+++ app/app.php
@@ -0,0 +1,3 @@
+<?php 
+echo "i am app.php"
+?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+
+
 <?php 
-echo "hello world";
+$root = (__DIR__. DIRECTORY_SEPARATOR);
+
+define("APP_PATH", $root . 'app' . DIRECTORY_SEPARATOR);
+print_r(APP_PATH . 'app.php'. PHP_EOL) ;
+
+require APP_PATH . 'app.php';
 ?>
