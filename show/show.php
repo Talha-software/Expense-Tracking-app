4d4f4e300c2c10994824e71bd4164a15d5a7308e
Talha Sajjad <talha.sajjad.software@gmail.com>
2024-08-13 22:21:38 +0500

    separeated the styling from original file

--- show/show.php
+++ show/show.php
@@ -5,7 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<link rel="stylesheet" href="../style.css">
+
+<style>
+    table {
+    width: 100%;
+    border-collapse: collapse;
+    text-align: center;
+}
+
+table tr th,
+table tr td {
+    padding: 5px;
+    border: 1px #eee solid;
+}
+
+tfoot tr th,
+tfoot tr td {
+    font-size: 20px;
+}
+
+tfoot tr th {
+    text-align: right;
+}
+</style>
 </head>
 
 <body>
